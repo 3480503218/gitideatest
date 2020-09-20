7dcdc7cd928a0782ecd68e6ad2c6c05352b2bf10
dhm <3480503218@qq.com>
2020-09-20 13:13:36 +0800

    git 更新2

--- githello/src/main/java/com/dhm.java
+++ githello/src/main/java/com/dhm.java
@@ -4,5 +4,6 @@ public class dhm {
     public static void main(String[] args) {
         System.out.println("git hello");
         System.out.println("更新1");
+        System.out.println("git 更新2");
     }
 }
