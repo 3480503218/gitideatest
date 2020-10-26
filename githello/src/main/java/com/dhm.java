e1b822a5af55ca1efb6903ddc7b89daa3ca540e2
3480503218 <49174564+3480503218@users.noreply.github.com>
2020-10-26 14:42:01 +0800

    Update dhm.java

--- githello/src/main/java/com/dhm.java
+++ githello/src/main/java/com/dhm.java
@@ -12,7 +12,7 @@ public class dhm {
         System.out.println("hebing03");
         System.out.println("克隆");
         System.out.println("kkas");
-        System.out.println("sgg");
+        System.out.println("sgge");
         System.out.println("sai");
     }
      
